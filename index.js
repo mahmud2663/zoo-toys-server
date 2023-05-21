0b17d73067df55227f95bcce4b0691569ba9b156
mahmud2663 <akibhasan119@gmail.com>
2023-05-21 19:46:07 +0600

    ok

--- index.js
+++ index.js
@@ -115,8 +115,8 @@ async function run() {
   }
 }
 
-run().catch(console.error);
-
-app.listen(port, () => {
+run().app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
-});
+}).catch(console.error);
+
+
